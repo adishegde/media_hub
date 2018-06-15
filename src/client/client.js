12ae6a4b2303bf9139a21ed49b4c70711ad85489
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-06-15 12:40:58 +0530

    Add file download functionality to Client
    Client allows file to be downloaded to a default directory or a specific
    path for each dowload. If resovled path is a directory then filename is
    taken from "Content-Disposition" header.
    
    It also supports progress by taking in a callback.

--- src/client/client.js
+++ src/client/client.js
@@ -1,8 +1,13 @@
 // Client makes the request
 
 import Dgram from "dgram";
-
+import * as Fs from "fs";
 import Winston from "winston";
+import * as Util from "util";
+import { get as http } from "http";
+import { URL } from "url";
+import * as Path from "path";
+
 import {
     DEFAULT_CLIENT as DEFAULT,
     DEFAULT_UDP_PORT,
@@ -11,6 +16,8 @@ import {
 } from "../utils/constants";
 
 const logger = Winston.loggers.get("client");
+const fstat = Util.promisify(Fs.stat);
+const fsaccess = Util.promisify(Fs.access);
 
 export default class Client {
     // Params:
@@ -24,20 +31,40 @@ export default class Client {
     //   be made. Default value is "255.255.255.255".
     //   - timeout [optional]: The time to listen for responses to udp
     //   requests. Default value is 3 seconds
+    //   - incoming [optional]: The default incoming directory for files. If
+    //   not given path must be provided when downloading.
     constructor({
         clientPort: port = DEFAULT.port,
         udpPort = DEFAULT_UDP_PORT,
         httpPort = DEFAULT_HTTP_PORT,
         network = DEFAULT_NETWORK,
         broadcastIp = DEFAULT.broadcastIp,
-        timeout = DEFAULT.timeout
+        timeout = DEFAULT.timeout,
+        incoming
     }) {
+        if (incoming) {
+            try {
+                // check if incoming is valid directory
+                let incomingStat = Fs.statSync(incoming);
+                if (!incomingStat.isDirectory()) {
+                    logger.debug(
+                        "Client: Incoming is not a directory. It will be ignored"
+                    );
+                    incoming = null;
+                }
+            } catch (err) {
+                logger.debug(`Client: ${err}`);
+                incoming = null;
+            }
+        }
+
         this.port = port;
         this.udpPort = udpPort;
         this.httpPort = httpPort;
         this.network = network;
         this.broadcastIp = broadcastIp;
         this.timeout = timeout;
+        this.incoming = incoming;
     }
 
     // Broadcast search request
@@ -52,7 +79,7 @@ export default class Client {
         return new Promise((resolve, reject) => {
             if (typeof searchString !== "string") {
                 logger.debug(
-                    `Client: searchString is of type ${typeof searchString}`
+                    `Client.search: searchString is of type ${typeof searchString}`
                 );
                 reject("searchString should be of type string");
             }
@@ -117,7 +144,7 @@ export default class Client {
 
                 let addr = socket.address();
                 logger.debug(
-                    `Client: search request for ${request.search}:${
+                    `Client.search: search request for ${request.search}:${
                         request.param
                     } sent via ${addr.address}:${addr.port}`
                 );
@@ -128,7 +155,7 @@ export default class Client {
                 // Add timeout for listening to responses
                 setTimeout(() => {
                     logger.debug(
-                        `Client: ${
+                        `Client.search: ${
                             this.timeout
                         } elapsed since search request. Timeout for response.`
                     );
@@ -136,5 +163,129 @@ export default class Client {
                 }, this.timeout);
             });
         });
+    }
+
+    // Download file
+    // Params:
+    //  - url: URL of file or directory to be downloaded.
+    //  - path [optional]: Path to save file. Can be name of file or directory. If not
+    //  given will be saved to incoming.
+    //  - callback [optional]: Called when progress is updated. The bytes
+    //  downloaded and file size will be passed as argument.
+    downloadFile(url, path, callback) {
+        // If path is falsy then try incoming
+        path = path || this.incoming;
+
+        // If callback is not function, ignore silently
+        if (typeof callback !== "function") {
+            logger.debug(
+                `Client.downloadFile: callback is of type ${typeof callbac}. Ignoring callback.`
+            );
+            // assign callback to empty function
+            callback = () => {};
+        }
+
+        if (!path) {
+            logger.debug(
+                "Client.downloadFile: No valid incoming or path given."
+            );
+            return Promise.reject("No valid incoming or path given.");
+        }
+        if (!url) {
+            logger.debug("Client.downloadFile: URL not given.");
+            return Promise.reject("URL not given.");
+        }
+
+        let pathIsDir = false;
+
+        // If path is directory
+        return fstat(path)
+            .then(
+                stat => {
+                    // if path is directory we are done
+                    if (stat.isDirectory()) {
+                        pathIsDir = true;
+                    }
+                },
+                err => {
+                    // path does not exist. Maybe it's the final path of the new file.
+                    // Check if parent directory exists.
+                    if (err.code === "ENOENT") {
+                        // Promise is rejected if parent directory doesn't exist
+                        return fstat(Path.dirname(path)).then(stat => {
+                            // if parent directory exists then ok else reject
+                            // Promise
+                            if (!stat.isDirectory()) {
+                                logger.debug(
+                                    "Client.download: Invalid path passed"
+                                );
+                                throw Error("Invalid path passed");
+                            }
+                        });
+                    } else {
+                        // Reject promise if any other error
+                        throw err;
+                    }
+                }
+            )
+            .catch(() => {
+                // In case of any error try and set path to incoming
+                if (!this.incoming) {
+                    // If incoming is falsy reject Promise
+                    throw Error("No valid incoming nor path passed.");
+                }
+                path = this.incoming;
+                pathIsDir = true;
+            })
+            .then(() => {
+                // If everything is ok then continue to download
+                return new Promise((resolve, reject) => {
+                    // Parsed url acts as config for http request
+                    http(`http://${url}`, res => {
+                        if (pathIsDir) {
+                            let fileName = res.headers["content-disposition"];
+                            // Extract filename from header. Assumption is that the
+                            // header is exactly as returned by the http service.
+                            // Might throw errors for other headers
+                            fileName = fileName.replace(
+                                `inline; filename="`,
+                                ""
+                            );
+                            // Remove trailing double quote
+                            fileName = fileName.slice(0, fileName.length - 1);
+
+                            path = Path.join(path, fileName);
+                        }
+
+                        let fileSize = parseInt(
+                            res.headers["content-length"],
+                            10
+                        );
+                        let bytesDownloaded = 0;
+
+                        callback(bytesDownloaded, fileSize);
+
+                        // If permission error occurs, error will be thrown here
+                        let downloadedFile = Fs.createWriteStream(path);
+                        // Write data
+                        res.pipe(downloadedFile);
+
+                        // Increase bytes downloaded for every chunk
+                        res.on("data", chunk => {
+                            bytesDownloaded += chunk.length;
+                            callback(bytesDownloaded, fileSize);
+                        });
+
+                        res.on("end", () => {
+                            // Resolve promise if download is completed
+                            resolve(path);
+                        });
+                        res.on("error", err => {
+                            logger.debug(`Client.downloadFile: ${err}`);
+                            reject(err);
+                        });
+                    });
+                });
+            });
     }
 }
