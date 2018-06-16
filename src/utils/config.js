fe18476746819c97922b3dd7bbb499fae87644cc
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-06-16 15:23:10 +0530

    Modify Config to store config data in _ property

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,6 @@
 import { readFileSync, writeFile } from "fs";
 import Winston from "winston";
 
-import { CONFIGKEYS } from "./constants";
-
 export default class Config {
     // Params:
     //  - filename: config file to parse
@@ -22,22 +20,15 @@ export default class Config {
             throw e;
         }
 
-        for (let key of CONFIGKEYS) {
-            this[key] = fileData[key];
-        }
+        this._ = fileData;
     }
 
     // Write config to file and return promise for the action
     write() {
-        let config = {};
-        for (let key of CONFIGKEYS) {
-            config[key] = this[key];
-        }
-
         // Return Promise that resolve when write file has been executed
         return new Promise((resolve, reject) => {
             // Resolve or reject promise in callback to writeFile
-            writeFile(this.filename, JSON.stringify(config), err => {
+            writeFile(this.filename, JSON.stringify(this._), err => {
                 if (err) reject();
                 else resolve();
             });
