9b24d23a44617ad004c52d4eaa99e1f9c964a035
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-06-17 10:32:46 +0530

    Filter search results based on shared directories
    This ensures that no invalid results are sent for searches even if meta
    data has information about other paths.

--- src/daemon/fileIndex/searchHandler.js
+++ src/daemon/fileIndex/searchHandler.js
@@ -6,28 +6,47 @@
  */
 
 import Fuse from "fuse.js";
+import * as Path from "path";
 import Winston from "winston";
 
 import { DEFAULT_SERVER as DEFAULT } from "../../utils/constants";
 
 const logger = Winston.loggers.get("daemon");
 
+// Returns true if path is descendent of any directory in dirList
+function isChild(path, dirList) {
+    return dirList.some(dir => {
+        let relative = Path.relative(dir, path);
+
+        return (
+            !!relative &&
+            !relative.startsWith("..") &&
+            !Path.isAbsolute(relative)
+        );
+    });
+}
 export default class SearchHandler {
     // Params:
     //  - metaDataHandler: Object of class MetaData
     //  - An object having the following properties:
     //    - maxResults [optional]: Number of results to return. If less than 0 all results
     //  will be returned else specified number
-    constructor(metaDataHandler, { maxResults = DEFAULT.maxResults }) {
+    //    - share: Array of shared directories
+    constructor(metaDataHandler, { maxResults = DEFAULT.maxResults, share }) {
         if (!metaDataHandler) {
             logger.error(
                 "MetaData object not passed to FileIndex constructor."
             );
             throw Error("MetaData object not passed to FileIndex constructor.");
         }
+        if (!share) {
+            logger.error("SearchHandler: share not passed.");
+            throw Error("Share not passed to SearchHandler.");
+        }
 
         this.metaData = metaDataHandler;
         this.maxResults = maxResults;
+        this.share = share;
     }
 
     // Fuzzy search on name of files
@@ -49,11 +68,7 @@ export default class SearchHandler {
         };
 
         let fuse = new Fuse(this.metaData.getFileList(), options);
-
-        let result = fuse.search(search);
-
-        if (this.maxResults < 0) return results;
-        return result.splice(0, this.maxResults);
+        return this._filterSearches(fuse.search(search));
     }
 
     // Fuzzy search on tags of files
@@ -72,11 +87,7 @@ export default class SearchHandler {
         };
 
         let fuse = new Fuse(this.metaData.getFileList(), options);
-
-        let result = fuse.search(search);
-
-        if (this.maxResults < 0) return results;
-        return result.splice(0, this.maxResults);
+        return this._filterSearches(fuse.search(search));
     }
 
     // Weighted fuzzy search on multiple properties
@@ -106,10 +117,18 @@ export default class SearchHandler {
         };
 
         let fuse = new Fuse(this.metaData.getFileList(), options);
+        return this._filterSearches(fuse.search(search));
+    }
 
-        let result = fuse.search(search);
+    _filterSearches(results) {
+        // Additional check to ensure that only those paths that are
+        // shared should be returned. Else invalid search results will served
+        // by UDP service
+        results = results.filter(result => {
+            return isChild(result.path, this.share);
+        });
 
-        if (this.maxResults < 0) return result;
-        return result.splice(0, this.maxResults);
+        if (this.maxResults < 0) return results;
+        return results.slice(0, this.maxResults);
     }
 }
