55b076100d1070e74837020d722e11957abcce5e
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-06-26 23:59:38 +0530

    Add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# first build using yarn
+yarn build
+
+buildPath="$PWD/build"
+binPath="$PWD/bin"
+
+osList='win macos linux'
+
+rm -r $binPath
+
+for os in $osList
+do
+	yarn run pkg "$buildPath/cli/daemon.js" --output "$binPath/$os-x64/daemon" --target "node8-$os-x64"
+	yarn run pkg "$buildPath/cli/client.js" --output "$binPath/$os-x64/client" --target "node8-$os-x64"
+	zip -r -X "$binPath/$os-x64.zip" "$binPath/$os-x64"
+done
