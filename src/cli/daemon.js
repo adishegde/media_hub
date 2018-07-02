1a44d5a1899ed087fd7dc2420515334d7350d444
Aditya Shridhar Hegde <adishegde@gmail.com>
2018-07-03 02:40:51 +0530

    cli: Update version to v0.4.0

--- src/cli/daemon.js
+++ src/cli/daemon.js
@@ -21,7 +21,7 @@ const logger = Winston.loggers.get("daemon");
 const share = [];
 const ignore = [...DEFAULT.ignore];
 
-Program.version("0.3.0")
+Program.version("0.4.0")
     .usage("[options]")
     .option(
         "-u, --udpPort <port>",
